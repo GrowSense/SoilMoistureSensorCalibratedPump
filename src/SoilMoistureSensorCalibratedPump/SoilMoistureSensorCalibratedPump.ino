d93c06ba21845548a513dcdf92a91a73aaff332f
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-29 04:15:40 +0000

    Removed obsolete eeprom clearing code

--- src/SoilMoistureSensorCalibratedPump/SoilMoistureSensorCalibratedPump.ino
+++ src/SoilMoistureSensorCalibratedPump/SoilMoistureSensorCalibratedPump.ino
@@ -24,11 +24,6 @@ void setup()
     Serial.println("Starting irrigator");
   }
 
-  for (int i = 0; i < EEPROM.length();i++)
-  {
-    EEPROM.write(i, 0);
-  }
-
   setupSoilMoistureSensor();
 
   setupIrrigation();
