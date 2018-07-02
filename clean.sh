8005877e59522dd6370a876bde5e74ebacd03af6
CompulsiveCoder <compulsivecoder@gmail.com>
2018-07-02 10:19:16 +1000

    Clean script now resets version

--- clean.sh
+++ clean.sh
@@ -1,1 +1,5 @@
 #pio run --target clean
+
+SOURCE_FILE="src/SoilMoistureSensorCalibratedPump/SoilMoistureSensorCalibratedPump.ino"
+
+sed -i "s/#define VERSION .*/#define VERSION \"1-0-0-0\"/" $SOURCE_FILE
