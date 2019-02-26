2afb77e2ff61af8995026557cc56ee54e971fcf8
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-26 08:33:33 +0000

    Trying to fix test

--- tests/nunit/src/SoilMoistureSensorCalibratedPump.Tests.Integration/HardwareTestHelper.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedPump.Tests.Integration/HardwareTestHelper.cs
@@ -195,9 +195,8 @@ namespace SoilMoistureSensorCalibratedPump.Tests.Integration
             // Give the pin some time at LOW to ensure reset
             Thread.Sleep (10);
 
-            // Change the reset trigger pin to an input to go back to normal
-            // TODO: Implement a cleaner way to do this
-            SimulatorClient.DigitalRead (ResetTriggerPin);
+            // Cancel the reset
+            SimulatorClient.PinMode (ResetTriggerPin, PinMode.INPUT_PULLUP);
 
             // Re-open the connection to the device
             ConnectDevice ();
