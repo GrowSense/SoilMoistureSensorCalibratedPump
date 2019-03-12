6c2bf04416e47ac24c154a8e12902876a9ec2772
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-12 06:17:23 +0000

    Fixed tests

--- tests/nunit/src/SoilMoistureSensorCalibratedPump.Tests.Integration/GreenSenseIrrigatorHardwareTestHelper.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedPump.Tests.Integration/GreenSenseIrrigatorHardwareTestHelper.cs
@@ -2,34 +2,34 @@
 
 namespace SoilMoistureSensorCalibratedPump.Tests.Integration
 {
-	public class GreenSenseIrrigatorHardwareTestHelper : GreenSenseHardwareTestHelper
-	{
-		public int SimulatorPumpPin = 2;
+    public class GreenSenseIrrigatorHardwareTestHelper : GreenSenseHardwareTestHelper
+    {
+        public int SimulatorPumpPin = 2;
 
-		public GreenSenseIrrigatorHardwareTestHelper()
-		{
-		}
+        public GreenSenseIrrigatorHardwareTestHelper ()
+        {
+        }
 
-		public override void PrepareDeviceForTest(bool consoleWriteDeviceOutput)
-		{
-			base.PrepareDeviceForTest(false);
+        public override void PrepareDeviceForTest (bool consoleWriteDeviceOutput)
+        {
+            base.PrepareDeviceForTest (false);
 
-			SetDevicePumpOffTime(0);
+            SetDevicePumpOffTime (0);
 
-			if (consoleWriteDeviceOutput)
-				ReadFromDeviceAndOutputToConsole();
-		}
+            if (consoleWriteDeviceOutput)
+                ReadFromDeviceAndOutputToConsole ();
+        }
 
-		public void SetDevicePumpOffTime(int pumpOffTime)
-		{
-			var cmd = "O" + pumpOffTime;
+        public void SetDevicePumpOffTime (int pumpOffTime)
+        {
+            var cmd = "O" + pumpOffTime;
 
-			Console.WriteLine("");
-			Console.WriteLine("Setting pump off time to " + pumpOffTime + " seconds...");
-			Console.WriteLine("  Sending '" + cmd + "' command to device");
-			Console.WriteLine("");
+            Console.WriteLine ("");
+            Console.WriteLine ("Setting pump off time to " + pumpOffTime + " seconds...");
+            Console.WriteLine ("  Sending '" + cmd + "' command to device");
+            Console.WriteLine ("");
 
-			SendDeviceCommand(cmd);
-		}
-	}
+            SendDeviceCommand (cmd);
+        }
+    }
 }
