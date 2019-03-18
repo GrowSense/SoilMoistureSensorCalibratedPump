58e28aef5af668e49bb03c5107d9c2755f14ef4c
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-18 06:03:13 +0000

    Reduced the amount of console output during setup

--- prepare.sh
+++ prepare.sh
@@ -2,25 +2,25 @@
 
 # Note: You may need to run this script with sudo
 
-echo "Preparing for project"
+echo "Preparing this system for the project"
 
 DIR=$PWD
 
-sudo apt-get update
+sudo apt-get update -qq
 
 # curl
 if ! type "curl" > /dev/null; then
-  sudo apt-get install -y curl
+  sudo apt-get install -qq -y curl
 fi
 
 # unzip
 if ! type "unzip" > /dev/null; then
-  sudo apt-get install -y unzip
+  sudo apt-get install -qq -y unzip
 fi
 
 # git
 if ! type "git" > /dev/null; then
-  sudo apt-get install -y git
+  sudo apt-get install -qq -y git
 fi
 
 sh prepare-sketch.sh
@@ -28,3 +28,5 @@ sh prepare-tests.sh
 
 
 cd $DIR
+
+echo "Finished preparing your project."
