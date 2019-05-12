228e64a5445c70172a6dfc7aad7c75e8bdcde241
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-12 14:46:37 +1000

    Updated install platformio script

--- install-platformio.sh
+++ install-platformio.sh
@@ -1,11 +1,55 @@
+SUDO=""
+if [ ! "$(id -u)" -eq 0 ]; then
+  if [ ! -f "is-mock-sudo.txt" ]; then
+    SUDO='sudo'
+  fi
+fi
+
 # python
 if ! type "python" > /dev/null; then
-  sudo apt-get -qq install -y python python-pip
+  echo "Installing python"
+
+  $SUDO apt-get install -y python
+fi
+
+# pip
+if ! type "pip" > /dev/null; then
+  echo "Installing python-pip"
+
+  $SUDO apt-get install -y python-pip
 fi
 
 # platform.io
 if ! type "pio" > /dev/null; then
-  python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
+  echo "Upgrading pip"
+  $SUDO pip install --ignore-installed --upgrade pip
+
+  echo "Installing pip extras"
+  pip install --ignore-installed --user setuptools wheel
+
+  echo "Installing platformio"
+  python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+  
+  # Give the user necessary permissions
+  $SUDO usermod -a -G tty $USER
+  $SUDO usermod -a -G dialout $USER
+
+else
+  echo "Platform.io is already installed. Skipping."
 fi
 
-platformio upgrade
+# If platformio install failed try again via pip
+if ! type "pio" > /dev/null; then
+  echo "Installing platformio via pip"
+  
+  pip install --ignore-installed -U platformio
+  
+  # Give the user necessary permissions
+  $SUDO usermod -a -G tty $USER
+  $SUDO usermod -a -G dialout $USER
+
+fi
+
+echo "Finished installing platformio"
+
+
