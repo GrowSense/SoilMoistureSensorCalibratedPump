cd916b4cb29cfcabb3f8520ca251ee79e1e32aad
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-27 07:07:45 +1000

    Updated/fixed graduate lts script

--- graduate-lts.sh
+++ graduate-lts.sh
@@ -8,28 +8,36 @@ fi
 
 echo "Graduating master branch to lts branch"
 
-# Fetch other branches
+echo ""
+echo "Fetching other branches from origin..."
 git fetch origin && \
 
-# Pull the master branch to update it
+echo ""
+echo "Pulling the master branch from origin (to update it locally)..."
 git pull origin master && \
 
-# Merge lts branch into the master branch
+echo ""
+echo "Merging the lts branch into the master branch..."
 git merge lts && \
 
-# Checkout the lts branch
+echo ""
+echo "Checking out the lts branch..."
 git checkout lts && \
 
-# Ensure it's up to date
+echo ""
+echo "Pulling the lts branch from origin to update it..."
 git pull origin lts && \
 
-# Merge the master branch
+echo ""
+echo "Merging the master branch into the lts branch..."
 git merge master && \
 
-# Push the updates
+echo ""
+echo "Pushing the updated lts branch to origin..."
 git push origin lts && \
 
-# Go back to the original branch
+echo ""
+echo "Checking out the $BRANCH again..."
 git checkout $BRANCH && \
 
 echo "The 'master' branch has been graduated to the 'lts' branch"
