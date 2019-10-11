39d4b55cd5c1b54999eef1e3fbb4218fd81c94d2
CompulsiveCoder <compulsivecoder@gmail.com>
2019-10-11 17:33:26 +1100

    Added missing files

---
+++ tests/nunit/src/SoilMoistureSensorCalibratedPump.Tests.Integration/GrowSenseIrrigatorHardwareTestHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SoilMoistureSensorCalibratedPump.Tests.Integration
+{
+    public class GrowSenseIrrigatorHardwareTestHelper : GrowSenseHardwareTestHelper
+    {
+        public int SimulatorPumpPin = 2;
+
+        public GrowSenseIrrigatorHardwareTestHelper ()
+        {
+        }
+
+        public override void PrepareDeviceForTest (bool consoleWriteDeviceOutput)
+        {
+            base.PrepareDeviceForTest (false);
+
+            SetDevicePumpOffTime (0);
+
+            if (consoleWriteDeviceOutput)
+                ReadFromDeviceAndOutputToConsole ();
+        }
+
+        public void SetDevicePumpOffTime (int pumpOffTime)
+        {
+            var cmd = "O" + pumpOffTime;
+
+            Console.WriteLine ("");
+            Console.WriteLine ("Setting pump off time to " + pumpOffTime + " seconds...");
+            Console.WriteLine ("  Sending '" + cmd + "' command to device");
+            Console.WriteLine ("");
+
+            SendDeviceCommand (cmd);
+        }
+    }
+}
